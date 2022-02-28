33fc228f0aa1d225bed7af95bfbe089ccd22680c
shawasty <shawasty@gmail.com>
2022-02-28 03:12:12 -0500

    created schema for employee with nested document

--- mongo_Hw/db/index.js
+++ mongo_Hw/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/employeeDatabase',{useUnifiedTopology:true, useNewUrlParser:true}).then(()=>{
+mongoose.connect('mongodb://127.0.0.1:27017/employeesDatabase',{useUnifiedTopology:true, useNewUrlParser:true}).then(()=>{
     console.log('successfuly connected to MongoDB');
 }).catch(e =>{
     console.error('connection error', e.message)
--- mongo_Hw/models/employee.js
+++ mongo_Hw/models/employee.js
@@ -2,12 +2,25 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
+const Employee_address = new Schema({
+    street: {type: String, required: true },
+    city: {type: String, required: true },
+    state: {type: String, required: true },
+    zip: {type: String, required: true },
+
+
+})
+
 const Employee = new Schema (
     {
         first_name: {type: String, required: true },
         last_name: {type: String, required: true },
         email: {type: String, required: true },
         job_title:{ type : String, required: true},
-    }
+        address : [Employee_address]
+    },
+    {timestamps:true}
 
 )
+
+module.exports = mongoose.model('employees', Employee)
